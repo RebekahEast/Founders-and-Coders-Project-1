7e8ef0a4889c3fe560cdbabfc6e3e618555ee51c
RebekahEast <rebekah.jean.east@gmail.com>
2023-02-03 16:11:26 +0000

    update on JS date

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const d = new Date();
-document.getElementById("date").innerHTML = d;
+const currentDate = new Date();
+
+const currentDayOfMonth = currentDate.getDate();
+
+const currentMonth = currentDate.getMonth(); 
+
+const currentYear = currentDate.getFullYear();
+
+const dateString = currentDayOfMonth + " - " + (currentMonth + 1) + " - " + currentYear;
+// + 1 because Jan is not 0
+
+document.getElementById("date").innerHTML = dateString;
