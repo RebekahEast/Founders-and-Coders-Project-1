0df2458c083978e24bdf9baf96d0cbedaa1d68de
RebekahEast <rebekah.jean.east@gmail.com>
2023-02-08 09:46:27 +0000

    date

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 1. Use querySelector to assign an HTML element to a variable
-// 2. Create an HTML element in JavaScript
-// 3. Append a Date()to an element existing in JavaScript
-// 4. Append a new element to an existing HTML element
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("date").innerHTML = d;
